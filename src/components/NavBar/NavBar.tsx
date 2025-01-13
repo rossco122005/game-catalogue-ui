7099d655c5ee871e6c35698d4421d802885efe25
Ross O'Neill <rossco122005@gmail.com>
2025-01-13 19:09:36 +0000

    Navbar added and displays on all pages

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,44 @@
+import { Link } from "react-router"
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+    navigationMenuTriggerStyle,
+  } from "@/components/ui/navigation-menu"
+
+function NavBar() {
+    return (
+    <div className="flex justify-center m-5">
+        <NavigationMenu>
+            <NavigationMenuList>
+                <NavigationMenuItem>
+                    <Link to='/'>
+                        <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                            Home
+                        </NavigationMenuLink>
+                    </Link>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                    <Link to='/login'>
+                        <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                            Login
+                        </NavigationMenuLink>
+                    </Link>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                    <Link to='/about'>
+                        <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                            About
+                        </NavigationMenuLink>
+                    </Link>
+                </NavigationMenuItem>
+            </NavigationMenuList>
+        </NavigationMenu>
+    </div>
+    )
+}
+
+export default NavBar
--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,42 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './App.css'
-import { createBrowserRouter, RouterProvider } from 'react-router'
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router'
 import HomePage from './pages/HomePage.tsx'
 import AboutPage from './pages/AboutPage.tsx'
 import NotFoundPage from './pages/NotFoundPage.tsx'
 import LoginPage from './pages/LoginPage.tsx'
+import NavBar from './components/NavBar/NavBar.tsx'
+
+const HeaderLayout = () => (
+  <>
+    <header>
+      <NavBar />
+    </header>
+    <Outlet />
+  </>
+);
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <HomePage />,
-    errorElement: <NotFoundPage/>
-  },
-  {
-    path: '/about',
-    element: <AboutPage />
-  },
-  {
-    path: '/login',
-    element: <LoginPage />
+    element: <HeaderLayout/>,
+    children: [
+      {
+        path: '/',
+        element: <HomePage />,
+        errorElement: <NotFoundPage/>
+      },
+      {
+        path: '/about',
+        element: <AboutPage />
+      },
+      {
+        path: '/login',
+        element: <LoginPage />
+      }
+    ]
   }
+  
 ])
 
 createRoot(document.getElementById('root')!).render(
